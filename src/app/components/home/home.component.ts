2cae56f1725d0b87c1276957c4f599ce471ac14a
serii <seriiburduja@gmail.com>
2024-02-21 10:04:51 +0200

    feat: HousingLocation interface

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,25 @@
 import {Component} from '@angular/core';
 import {HousingLocationComponent} from '../housing-location/housing-location.component';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HousingLocationComponent],
+  imports: [HousingLocationComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
+  housingLocation: HousingLocationComponent = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
