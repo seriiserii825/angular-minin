06b562ee8b01595b2b4d1179d5f16f1c5322d181
serii <seriiburduja@gmail.com>
2024-02-21 10:40:26 +0200

    feat: housing grid

--- src/app/components/housing-location/housing-location.component.ts
+++ src/app/components/housing-location/housing-location.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { Component, Input } from '@angular/core';
+import {Ihosinglocation} from '../../interfaces/ihosinglocation';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.scss'
 })
 export class HousingLocationComponent {
-
+ @Input() housing_location!: Ihosinglocation;
 }
