9bd7441535c50ad3619d6e394e73e3b3bce91975
serii <seriiburduja@gmail.com>
2024-02-22 10:39:01 +0200

    feat: heroes click

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,16 +1,20 @@
 import {Component} from '@angular/core';
 import {Ihero} from '../../interfaces/ihero';
+import {FormsModule} from '@angular/forms';
+import {HEROES} from './mock-heroes';
+import {NgFor, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgFor, NgIf],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss'
 })
 export class HeroesComponent {
-  hero: Ihero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selected_hero: Ihero | undefined;
+  heroes: Ihero[] = HEROES;
+  onSelect(hero: Ihero): void {
+    this.selected_hero = hero;
+  }
 }
